e338c4646c1fb7f91934a31271f8aa17bf150333
Nat <nathanianah@gmail.com>
2016-11-06 17:13:04 -0500

    Minor Edits/Deletions
    Renamed sections and deleted several irrelevant blocks of code

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# nathanianah.github.io
+# JCA Congregational Retreat Microsite
+_nathanianah.github.io_
+
+**To Do**
+1. Staff Information (Speaker TBD)
+1. Google Forms (Registration)
+1. Create Video Section
+1. Select Background Images
+
+**Bugs**
+1. "Register" image not appearing
+1. Need to clean up code
+
+#######The template was copied/pasted from a variety of sources, so much of the code is irrelevant/unnecessary/needs to be pruned.
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -3,12 +3,12 @@
 -------------------------------------------------------------------*/
 var  pageTopImage = jQuery('#page-top').data('background-image');
 var  aboutImage = jQuery('#about').data('background-image');
-var  subscribeImage = jQuery('#subscribe').data('background-image');
+var  registerImage = jQuery('#register').data('background-image');
 var  contactImage = jQuery('#contact').data('background-image');
 
 if (pageTopImage) {  jQuery('#page-top').css({ 'background-image':'url(' + pageTopImage + ')' }); };
 if (aboutImage) {  jQuery('#about').css({ 'background-image':'url(' + aboutImage + ')' }); };
-if (subscribeImage) {  jQuery('#subscribe').css({ 'background-image':'url(' + subscribeImage + ')' }); };
+if (registerImage) {  jQuery('#register').css({ 'background-image':'url(' + registerImage + ')' }); };
 if (contactImage) {  jQuery('#contact').css({ 'background-image':'url(' + contactImage + ')' }); };
 
 /* Background Images End
@@ -87,7 +87,7 @@ jQuery(document).ready(function($) {
     /* Next Section   
 	-------------------------------------------------------------------*/
 	$('.next-section .go-to-about').click(function () {$('html,body').animate({ scrollTop: $('#about').offset().top }, 1000);});
-	$('.next-section .go-to-subscribe').click(function () {$('html,body').animate({ scrollTop: $('#subscribe').offset().top }, 1000);});
+	$('.next-section .go-to-register').click(function () {$('html,body').animate({ scrollTop: $('#register').offset().top }, 1000);});
 	$('.next-section .go-to-contact').click(function () {$('html,body').animate({ scrollTop: $('#contact').offset().top }, 1000);});
 	$('.next-section .go-to-page-top').click(function () {$('html,body').animate({ scrollTop: $('#page-top').offset().top }, 1000);});
 
@@ -97,22 +97,11 @@ jQuery(document).ready(function($) {
 
 
 
-    /* Subscribe
+    /* Register
     -------------------------------------------------------------------*/
-    $(".news-letter").ajaxChimp({
-        callback: mailchimpResponse,
-        url: "http://jeweltheme.us10.list-manage.com/subscribe/post?u=a3e1b6603a9caac983abe3892&amp;id=257cf1a459" // Replace your mailchimp post url inside double quote "".  
-    });
-
-    function mailchimpResponse(resp) {
-         if(resp.result === 'success') {
-         
-            $('.alert-success').html(resp.msg).fadeIn().delay(3000).fadeOut();
-            
-        } else if(resp.result === 'error') {
-            $('.alert-warning').html(resp.msg).fadeIn().delay(3000).fadeOut();
-        }  
-    };
+    //insert google forms magic here
+    /* Register End
+	-------------------------------------------------------------------*/
 
 
 
